bc4be9e19c29c69aba51535f74f657b690d404bb
buffet <niclas@countingsort.com>
2022-09-26 16:04:20 +0000

    fix(soju): use soju without nginx

--- programs/soju.nix
+++ programs/soju.nix
@@ -1,6 +1,5 @@
-_: let
+{config, ...}: let
   port = 6697;
-  internal_port = 6698;
 in {
   networking.firewall.allowedTCPPorts = [port];
 
@@ -9,35 +8,13 @@ in {
     certs."irc.buffet.sh".email = "niclas@countingsort.com";
   };
 
-  services = {
-    soju = {
-      enable = true;
-      hostName = "irc.buffet.sh";
-      listen = ["irc+insecure://:${toString internal_port}"];
-    };
-
-    nginx = {
-      enable = true;
-
-      recommendedGzipSettings = true;
-      recommendedOptimisation = true;
-      recommendedProxySettings = true;
-      recommendedTlsSettings = true;
-
-      virtualHosts."irc.buffet.sh" = {
-        enableACME = true;
-        forceSSL = true;
-        listen = [
-          {
-            addr = "irc.buffet.sh";
-            inherit port;
-          }
-        ];
-
-        locations."/" = {
-          proxyPass = "http://localhost:${toString internal_port}";
-        };
-      };
-    };
+  services.soju = let
+    certdir = config.security.acme.certs."irc.buffet.sh".directory;
+  in {
+    enable = true;
+    hostName = "irc.buffet.sh";
+    listen = [":${toString port}"];
+    tlsCertificate = "${certdir}/cert.pem";
+    tlsCertificateKey = "${certdir}/key.pem";
   };
 }
