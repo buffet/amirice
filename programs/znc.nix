3403d20923325598ec8b22a3e066f6716998f21a
buffet <niclas@countingsort.com>
2022-10-02 20:22:32 +0000

    feat(znc): use acme certs

--- programs/znc.nix
+++ programs/znc.nix
@@ -1,5 +1,10 @@
-_: let
+{
+  config,
+  pkgs,
+  ...
+}: let
   port = 4124;
+  certdir = "/var/lib/znc/certs";
 in {
   networking.firewall.allowedTCPPorts = [port];
 
@@ -10,7 +15,11 @@ in {
     config = {
       LoadModule = ["webadmin"];
 
+      SSLCertFile = "${certdir}/cert.pem";
+      SSLKeyFile = "${certdir}/key.pem";
+
       Listener.l = {
+        Host = "irc.buffet.sh";
         Port = port;
         SSL = true;
         IPv4 = true;
@@ -28,5 +37,22 @@ in {
         Pass = "sha256#d271536ea81296ebc9914b295c60024231a7beb4a05a1416485d42b0af3cc9ab#47DUOGLVcHKp7sJGwy:_#";
       };
     };
+  };
+
+  systemd.services.bind-znc-certs = {
+    description = "mount certs for znc with different perms";
+    after = ["local-fs.target"];
+    wantedBy = ["znc.service"];
+    serviceConfig.Type = "forking";
+
+    preStart = ''
+      ${pkgs.coreutils}/bin/mkdir -p ${certdir}
+    '';
+
+    script = ''
+      ${pkgs.bindfs}/bin/bindfs -u znc -g znc -r -p 0400,u+D \
+        ${config.security.acme.certs."buffet.sh".directory}    \
+        ${certdir}
+    '';
   };
 }
